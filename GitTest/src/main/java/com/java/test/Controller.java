d2ec575b8cc037abdd4efbf5eb55c0086c1a8cc2
Rajiv Ranjan <rajivranjanmca07@gmail.com>
2020-09-01 06:27:30 +0530

    First commit

---
+++ GitTest/src/main/java/com/java/test/Controller.java
@@ -0,0 +1,15 @@
+package com.java.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/org")
+public class Controller {
+
+	@GetMapping(value="/get")
+	public String getMsg() {
+		return "Hello Rajiv welcome to git";
+	}
+}
